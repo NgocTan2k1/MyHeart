4c1b71ce0f97d05868a28badead60daa2fdfefae
NgocTan <phamngoctan2016@gmail.com>
2023-03-05 01:13:56 +0700

    fixed audio play

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-var elementAudio = document.getElementById("audioMusic");
-elementAudio.play();
-
 var elementImg = document.getElementById("item-img");
 elementImg.innerHTML = `
     <img class="item" onclick="clickChangeImgMyLove()" src="./img/0.jpg" alt="">
     <p class="text">Click vô ảnh mà xem cái bạn xinh xinh này tiếp nè ^^</p>
 `;
+
+var elementAudio = document.getElementById("audioMusic");
+elementAudio.play();
 
 var elementClick = document.getElementById("btn-click");
 
